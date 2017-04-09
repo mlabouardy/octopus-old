7668add1cbc2baf10d44f3bb939ef4e94287a049
mlabouardy <mohamed@labouardy.com>
2017-04-09 19:32:12 +0200

    concat js files & inject into index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var del = require("del")
+var del = require('del');
+var series = require('stream-series');
+var inject = require('gulp-inject');
+var gulpSync = require('gulp-sync')(gulp);
 
 var input = {
   sass: 'public/assets/sass/**/*.scss',
-  html: ['!public/lib', 'public/**/*.html']
+  html: ['!public/plugins', 'public/**/*.html'],
+  js: 'public/app/**/*.js',
+  index: 'public/index.html'
 }
 
 var output = {
   css : 'build/assets/stylesheets',
-  html: 'build'
+  js: 'build/assets/javascript',
+  html: 'build',
+  index: 'build/index.html'
 }
 
 gulp.task('clean', function(cb){
@@ -29,4 +36,17 @@ gulp.task('build-html', function(){
              .pipe(gulp.dest(output.html))
 })
 
-gulp.task('build', ['build-html', 'build-css'])
+gulp.task('build-js', function(){
+  return gulp.src(input.js)
+             .pipe(concat('bundle.js'))
+             .pipe(gulp.dest(output.js))
+})
+
+gulp.task('inject', function(){
+  var sources = gulp.src([output.css + '/**/*.css', output.js + '/**/*.js'], {read:false})
+  return gulp.src(output.index)
+             .pipe(inject(sources), {ignorePath: input.html})
+             .pipe(gulp.dest(output.index))
+})
+
+gulp.task('build', gulpSync.sync(['build-html', 'build-css', 'build-js', 'inject']))
---
+++ public/app/controllers/home.controller.js
@@ -0,0 +1,6 @@
+var HomeCtrl = function(){
+  console.log('Home controller');
+};
+
+angular.module('app.controllers')
+  .controller('HomeCtrl', HomeCtrl);
