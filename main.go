a51a2bd3bdd644d291cdd8b620f0b497360ba9c4
mlabouardy <mohamed@labouardy.com>
2017-04-12 14:08:19 +0200

    golang template engine example

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
+/*package main
+
+import "html/template"
+
+func main() {
+
+	t := template.New("create template")
+	t, _ := t.ParseFiles("tmpl/telegraf.conf", nil)
+	r := mux.NewRouter()
+	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./build/"))))
+	err := http.ListenAndServe("localhost:4000", r)
+	if err != nil {
+		panic(err)
+	}
+}
+*/
 package main
 
 import (
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"os"
+	"text/template"
 )
 
+type Telegraf struct {
+	Hostname string
+	IsCpu    bool
+	IsDisk   bool
+	IsNet    bool
+	IsMem    bool
+	IsSwap   bool
+	IsSystem bool
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./build/"))))
-	err := http.ListenAndServe("localhost:5000", r)
+	config := Telegraf{Hostname: "vm-test", IsCpu: true, IsDisk: true, IsNet: true, IsMem: true, IsSwap: false, IsSystem: false}
+	tmpl, err := template.New("telegraf.conf").ParseFiles("tmpl/telegraf.conf")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, config)
 	if err != nil {
 		panic(err)
 	}
